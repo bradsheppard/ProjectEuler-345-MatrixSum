1c092455769ec66f86994dd43c277b6903c285de
Brad Sheppard <bradsheppard22@gmail.com>
2018-03-04 11:47:41 -0330

    Initial commit

---
+++ matrix.h
@@ -0,0 +1,18 @@
+//
+// Created by brad on 28/02/18.
+//
+
+#include <stdio.h>
+
+#ifndef PROJECTEULER_MATRIXSUM_MATRIX_H
+#define PROJECTEULER_MATRIXSUM_MATRIX_H
+
+#endif
+
+typedef struct {
+    unsigned int size;
+    int **values;
+} matrix;
+
+matrix *get_matrix(FILE *matrix_file);
+int get_max_sum(matrix *matrix_struct);
